9cf089a57ddfe009c0ab0f474694501428919899
gregalletti <gregorio.galletti@mail.polimi.it>
2022-05-07 15:39:27 +0200

    Setup commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          DIO MOSTRO!
         </p>
         <a
           className="App-link"
